a814b35d4ce11aef187bfb21c6c16b8b170a8245
jagrati gupta <123jagratigupta@gmail.com>
2024-12-20 18:19:39 +0530

    Jg: Added router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router-dom';
+import HomeComponent from './components/Home';
+import ProductsComponent from './components/Products';
+import FavoritesComponent from './components/Favorites';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomeComponent />,
+      },
+      {
+        path: "/products",
+        element: <ProductsComponent />,
+      },
+      {
+        path: "/favorites",
+        element: <FavoritesComponent />,
+      },
+    ],
+  },
+]);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
